d842cc30e93919439002ed96106452552ee74a37
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-10-30 02:04:09 +0000

    [kadai3]エラーちょっと解消

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  alidates :name, uniqueness: true, length: { in: 2..20 }
+  validates :name, uniqueness: true, length: { in: 2..20 }
   validates :introduction, length: { maximum: 50 }
 
 
