6727bd0b41b6edc32574aac2969ed12d2e7ffd0e
EC2 Default User <ec2-user@ip-172-31-38-164.ap-northeast-1.compute.internal>
2023-11-01 13:15:20 +0000

    [kadai3]テストエラー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      redirect_to user_path(@user.id)
       # 保存成功の処理
     else
       # 保存失敗の処理
